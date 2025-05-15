a4fb54bb8d2d424d5c431ac1b363654ae5232cd4
Michael <49960801+WhyIsThisMyUsername@users.noreply.github.com>
2025-05-15 12:46:23 +1200

    Change nvim tab settings

--- nvim/init.lua
+++ nvim/init.lua
@@ -161,6 +161,9 @@ vim.opt.scrolloff = 10
 -- See `:help 'confirm'`
 vim.opt.confirm = true
 
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
